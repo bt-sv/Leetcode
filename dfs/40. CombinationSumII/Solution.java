33130df108c9c7f38c3f6eb4a054693328932ac6
bt-sv <taobotong1987@gmail.com>
2021-07-06 22:32:01 -0700

    Update Solution.java

--- dfs/40. CombinationSumII/Solution.java
+++ dfs/40. CombinationSumII/Solution.java
@@ -1,1 +1,30 @@
-
+Class Solution {
+    List<Integer> list = new ArrayList<>();
+    List<List<Integer>> res = new ArrayList<>();
+    int[] candidates;
+    int target;
+    
+    public void dfs(int p, int k) {
+        if (k > target)
+            return;
+        if (k == target) {
+            res.add(new ArrayList<>(list));
+            return;
+        }
+        for (int i = p; i < candidates.length; i++) {
+            if (i != p && candidates[i] == candidates[i - 1])
+                continue;
+            list.add(candidates[i]);
+            dfs(i + 1, k + candidates[i]_);
+            list.remove(list.size() - 1);
+        }
+     }
+    
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        this.candidates = candidates;
+        this.target = target;
+        Arrays.sort(candidates);
+        dfs(0, 0);
+        return res;
+    }
+}
